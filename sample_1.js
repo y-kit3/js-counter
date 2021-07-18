3a83a818cad53a935b962f317d7a358f168402e4
y-kit3 <86399381+y-kit3@users.noreply.github.com>
2021-07-18 10:03:16 +0900

    Create sample_1.js

---
+++ sample_1.js
@@ -0,0 +1,38 @@
+(function () {
+    
+    const text = document.getElementById ("text");
+    const up_button = document.getElementById ("btn_up");
+    const down_button = document.getElementById ("btn_down");
+    const reset_button = document.getElementById ("btn_reset");
+    var count_value = 0;
+
+    console.log ('count_value =>',count_value);
+
+    //ボタンが押されたらカウント増
+    up_button.onclick = function (){
+        count_value += 1;
+        text.innerHTML = count_value;
+        console.log ('count_value =>',count_value);
+    };
+
+    //ボタンが押されたらカウント減
+    down_button.onclick = function (){
+        if  (count_value >= 1) {
+            count_value -= 1;
+            text.innerHTML = count_value;
+            console.log ('count_value =>',count_value);
+            };       
+    };
+
+    //ボタンが押されたら0に戻る
+    reset_button.onclick = function (){
+        count_value = 0;
+        text.innerHTML = count_value;
+        console.log ('count_value =>',count_value);
+    };
+})();
+
+
+
+
+
