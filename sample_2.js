f9d5324c1a5c1f6e0f1e0f20a04188866c6556ae
y-kit3 <86399381+y-kit3@users.noreply.github.com>
2021-07-18 11:16:26 +0900

    Update sample_2.js

--- sample_2.js
+++ sample_2.js
@@ -7,31 +7,29 @@
     var count_value = 0;
 
     console.log ('count_value =>',count_value);
-    console.log ("up_button =>",up_button);
-    console.log ("down_button =>",down_button);
-    console.log ("reset_button =>",reset_button);
 
     //ボタンが押されたらカウント増
-    up_button.onclick = function (){
-        count_value += 1;
+    up_button.addEventListener('click',(event) =>{
+        count_value++;
         text.innerHTML = count_value;
-        console.log('count_value =>' & count_value);
-    };
+        console.log('count_value =>',count_value);
+    })
 
     //ボタンが押されたらカウント減
-    down_button.onclick = function (){
-        if  (count_value >= 1) {
-            count_value -= 1;
+    down_button.addEventListener('click',(event) =>{
+        if (count_value >= 1) {
+            count_value--;
             text.innerHTML = count_value;
-            console.log('count_value =>' & count_value);
-            };       
-    };
+            console.log('count_value =>',count_value);
+        }
+    })
 
     //ボタンが押されたら0に戻る
-    reset_button.onclick = function (){
+    reset_button.addEventListener('click',(event) =>{
         count_value = 0;
         text.innerHTML = count_value;
-    };
+        console.log('count_value =>',count_value);
+    })
 })();
 
 
